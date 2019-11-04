d5ef03d546954f4384db5badf9efc57e815e346d
kazeem711 <55092919+kazeem711@users.noreply.github.com>
2019-11-04 11:50:29 -0500

    Update Items.java

--- Items.java
+++ Items.java
@@ -10,29 +10,75 @@ import java.util.HashMap;
  */
 public class Items
 {
-    // instance variables
-    private String describe;
+   // instance variables 
+    private String description;
     private int weight;
     private ArrayList<Items> items;
-    
-    /**
-     * Constructor for objects of class Items
-     * @param describe
-     * @param weight
-     */
-    public Items(String describe, int weight){
-        //initialize instance variables 
-        describe = describe;
+       /**
+         * Constructor for objects of class Item
+         * Create an item described "description", and Weight
+         */
+    public Items(String description, int weight)
+    {
+        // initialise instance variables
+        description = description;
         weight = weight;
         this.items = new ArrayList<Items>();
     }
     
+    
     /**
-     * Return description of the item
+     * Return the description of the item
      */
-    public String getShortDescription(){
-        return describe;
+    public String getShortDescription()
+    {
+        return description;
     }
     
+    /**
+     * Return the weight of the item
+     */
+    public int weight()
+    {
+        return weight;
+    }
+    
+    /**
+     * Return a long description of the item that includes the description 
+     * and weight.
+     */
+    public String getLongDescription()
+    {
+        return "Item " + description + " Weight " + weight;
+    }
+    
+    /**
+     * create a variety of items
+     */
+    public void add(Items item)
+    {
+        items.add(item);
+    }
+    
+    
+    /**
+     * get the item
+     */
+    public ArrayList<Items> getItems()
+    {
+        return items;
+    }
+    
+    /**
+     * 
+     */
+    public String getItemString()
+    {
+        String returnString = "Item:";
+        {
+            returnString += " " + items;
+        }
+        return returnString;
+    }
     
 }
